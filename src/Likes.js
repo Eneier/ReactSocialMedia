960fa4a9ee7697c7809865bb3af4ff00fa9f297c
Eneier <canadacoder94@gmail.com>
2022-07-29 13:11:13 -0400

    Second Commit Buttons, store, likes and logic

---
+++ src/Likes.js
@@ -0,0 +1,34 @@
+import {connect } from 'react-redux'
+
+function Likes(props) {
+
+    console.log('render', props)
+    return (
+        <div className='button-controls'>
+            <button onClick={props.onIncrementLikes}>❤ {props.likes}</button>
+            <button onClick={props.onDecrementLikes}>Dislike</button>
+        </div>
+    )
+}
+
+function mapStateToProps(state) {
+    console.log('mapStateToProps', state)
+    return {
+        likes: state.likes
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        onIncrementLikes: () => {
+            const action = {type: 'INCREMENT'}
+            dispatch(action)
+        },
+        onDecrementLikes: () => {
+            const action = {type: 'DECREMENT'}
+            dispatch(action)
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Likes)
