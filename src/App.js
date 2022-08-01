21560cb8fea16bb3982473a70f1d4ed79cc2bcd2
Eneier <canadacoder94@gmail.com>
2022-08-01 18:00:01 -0400

    Actions, Input and Reducer and Hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Likes from './Likes'
-
-
+import Title from "./Title";
 
 function App() {
   return (
@@ -10,6 +9,7 @@ function App() {
         <div className="card">
           <div className="card-image">
             <img src="./sea.jpg" alt="surfing"/>
+            <Title/>
               <Likes />
           </div>
         </div>
--- src/Likes.js
+++ src/Likes.js
@@ -1,8 +1,8 @@
 import {connect } from 'react-redux'
+import {decrementLikes, incrementLikes} from "./redux/actions";
+import {likesReducer} from "./redux/likesReducer";
 
 function Likes(props) {
-
-    console.log('render', props)
     return (
         <div className='button-controls'>
             <button onClick={props.onIncrementLikes}>❤ {props.likes}</button>
@@ -12,23 +12,17 @@ function Likes(props) {
 }
 
 function mapStateToProps(state) {
-    console.log('mapStateToProps', state)
-    const { likes } = state
+    const { likesReducer } = state
     return {
-        likes: likes.likes
+        likes: likesReducer.likes
+
     }
 }
 
 function mapDispatchToProps(dispatch) {
     return {
-        onIncrementLikes: () => {
-            const action = {type: 'INCREMENT'}
-            dispatch(action)
-        },
-        onDecrementLikes: () => {
-            const action = {type: 'DECREMENT'}
-            dispatch(action)
-        }
+        onIncrementLikes: () => dispatch(incrementLikes()),
+        onDecrementLikes: () => dispatch(decrementLikes())
     }
 }
 
