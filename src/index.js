e9802ba7635c57bdea82880784d3ac15f6e67e6f
Eneier <canadacoder94@gmail.com>
2022-09-07 16:53:14 -0400

    new middleware's

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore, applyMiddleware, compose} from "redux";
+// import {createStore, applyMiddleware, compose} from "redux";
 import {rootReducer} from "./redux/rootReducer";
 import {Provider} from "react-redux";
-import thunk from "redux-thunk";
 import {configureStore} from "@reduxjs/toolkit";
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 
 const store = configureStore({
     reducer: rootReducer,
-    thunk: thunk,
-    dev: window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    middleware: [thunk, logger],
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
+
+// dev: window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 ReactDOM.render(
     <Provider store={store}>
